16836c761781264f9e5422435f291673737e40b8
DESKTOP-1NG9R1A\yorda <yordan.detelinov@gmail.com>
2018-11-11 12:03:49 +0200

    git

---
+++ config/chatrooms.js
@@ -0,0 +1,19 @@
+module.exports = [
+  {
+    name: 'Terminus',
+    image: 'chatrooms/sql.jpg'
+  }
+  // {
+  //   name: 'Alexandria',
+  //   image: 'chatrooms/alexandria.jpg'
+  // },
+  // {
+  //   name: 'Sanctuary',
+  //   image: 'chatrooms/sanctuary.jpg'
+  // },
+  // {
+  //   name: 'Hilltop',
+  //   image: 'chatrooms/hilltop.jpg'
+  // }
+]
+
